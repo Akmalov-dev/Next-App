638ff4e38f5e0f7e44abd718d26922a3c6ce32ce
Akmalov-dev <amirbekdebruyne@gmail.com>
2026-07-17 11:00:13 +0500

    new

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Agar ushbu blok yoqilgan bo'lsa, muammo shundan bo'lishi mumkin
+  experimental: {
+    // optimizeCss: true, <-- buni o'chirib turing
+  },
+}
 
 module.exports = nextConfig
